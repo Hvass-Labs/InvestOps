48bb8ca5ce9f3e319dba3d9316ef2f46d7409139
Magnus <magnus@hvass-labs.org>
2021-10-14 10:39:38 +0100

    Added setup.py

---
+++ investops/__init__.py
@@ -0,0 +1,5 @@
+# The version is also defined in setup.py and must be updated in both places.
+__version__ = "0.0.1"
+
+# Expose the following as top-level imports.
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+# Also defined in investops/__init__.py and must be updated in both places.
+MY_VERSION = '0.0.1'
+
+setup(
+    name='investops',
+    packages=['investops'],
+    version=MY_VERSION,
+    description='Tools for investing in Python',
+    long_description='InvestOps is a collection of Python tools for investing, '
+                     'including the fast and robust \"Hvass Diversification\" '
+                     'algorithm for diversifying an investment portfolio.',
+    long_description_content_type="text/markdown",
+    author='Magnus Erik Hvass Pedersen',
+    author_email='my-first-name@hvass-labs.org',
+    url='https://github.com/Hvass-Labs/InvestOps',
+    license='MIT',
+    keywords=['investing', 'portfolio optimization'],
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
+
