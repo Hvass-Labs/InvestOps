54f5467095d9d81175cb4e81f9996641bf82d86d
Magnus <magnus@hvass-labs.org>
2022-01-26 10:37:02 +0000

    random.py added named weights

--- investops/random.py
+++ investops/random.py
@@ -15,12 +15,71 @@
 ###############################################################################
 
 import numpy as np
+import pandas as pd
 from investops.check import fix_corr_matrix
+
+###############################################################################
+# Generate default names.
+
+def gen_names_assets(num_assets):
+    """
+    Generate list of strings with default asset-names.
+
+    :param num_assets: Integer with the number of assets.
+    :return: List of strings.
+    """
+    return [f'Asset {i}' for i in range(num_assets)]
+
+
+def gen_names_groups(num_groups):
+    """
+    Generate list of strings with default group-names.
+
+    :param num_groups: Integer with the number of groups.
+    :return: List of strings.
+    """
+    return [f'Group {i}' for i in range(num_groups)]
+
+###############################################################################
+# Helper-functions.
+
+def _named_weights(weights, num_assets, names):
+    """
+    If `names` is not `None` then convert `weights` to a Pandas Series
+    either using the given list of asset-names, or generate default names.
+
+    :param weights: Numpy array with portfolio weights.
+    :param num_assets: Integer with the number of assets.
+    :param names:
+        If `None` then don't use names for the weights.
+        If `True` then generate default names for the asset-weights.
+        If list then use those names for the asset-weights.
+    :return:
+        If `names` is `None` then a Numpy array, otherwise a Pandas Series.
+    """
+    # Use names for the weights?
+    if names is not None:
+        if names is True:
+            # Use default asset-names.
+            names = gen_names_assets(num_assets=num_assets)
+        elif isinstance(names, list):
+            # Use the list of names as-is, assuming they are all strings.
+            # Check the number of names is correct.
+            if len(names) != num_assets:
+                msg = f'Argument \'names\' has wrong length {len(names)} ' \
+                      f'expected {num_assets}.'
+                raise ValueError(msg)
+
+        # Convert from Numpy array to Pandas Series.
+        weights = pd.Series(data=weights, index=names)
+
+    return weights
 
 ###############################################################################
 # Random Weights.
 
-def rand_weights_uniform(rng, num_assets, min_weight=0.0, max_weight=1.0):
+def rand_weights_uniform(rng, num_assets,
+                         min_weight=0.0, max_weight=1.0, names=None):
     """
     Generate random portfolio weights using a uniform distribution.
 
@@ -28,13 +87,20 @@ def rand_weights_uniform(rng, num_assets, min_weight=0.0, max_weight=1.0):
     :param num_assets: Integer with the number of portfolio assets.
     :param min_weight: Minimum portfolio weight.
     :param max_weight: Maximum portfolio weight.
-    :return: Numpy array with random weights.
+    :param names:
+        If `None` then don't use names for the weights.
+        If `True` then generate default names for the asset-weights.
+        If list then use those names for the asset-weights.
+    :return:
+        If `names` is `None` then return a Numpy array with random weights.
+        Otherwise return a Pandas Series with named random weights.
     """
-    return rng.uniform(low=min_weight, high=max_weight, size=num_assets)
+    weights = rng.uniform(low=min_weight, high=max_weight, size=num_assets)
+    return _named_weights(weights=weights, num_assets=num_assets, names=names)
 
 
 def rand_weights_normal(rng, num_assets, mean=0.0, std=0.04,
-                        min_weight=0.0, max_weight=1.0):
+                        min_weight=0.0, max_weight=1.0, names=None):
     """
     Generate random portfolio weights using a normal distribution that
     are also clipped between the given `min_weight` and `max_weight`.
@@ -45,7 +111,13 @@ def rand_weights_normal(rng, num_assets, mean=0.0, std=0.04,
     :param std: Std.dev. of the normal distribution.
     :param min_weight: Minimum portfolio weight.
     :param max_weight: Maximum portfolio weight.
-    :return: Numpy array with random weights.
+    :param names:
+        If `None` then don't use names for the weights.
+        If `True` then generate default names for the asset-weights.
+        If list then use those names for the asset-weights.
+    :return:
+        If `names` is `None` then return a Numpy array with random weights.
+        Otherwise return a Pandas Series with named random weights.
     """
     # Generate random portfolio weights.
     weights = rng.normal(scale=std, loc=mean, size=num_assets)
@@ -53,8 +125,7 @@ def rand_weights_normal(rng, num_assets, mean=0.0, std=0.04,
     # Clip the portfolio weights.
     weights = np.clip(weights, min_weight, max_weight)
 
-    return weights
-
+    return _named_weights(weights=weights, num_assets=num_assets, names=names)
 
 ###############################################################################
 # Random correlation matrices.
@@ -101,9 +172,7 @@ def rand_corr_normal(rng, num_assets, mean=0.0, std=0.2):
 
     return corr
 
-
 ###############################################################################
-
 
 def rand_zero(rng, a, prob):
     """
@@ -119,6 +188,5 @@ def rand_zero(rng, a, prob):
 
     # Create a copy of the input array where random values are set to zero.
     return np.where(p < prob, 0, a)
-
 
 ###############################################################################
