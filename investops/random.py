8194e46c212a785a64ec2c70d3192302794ae9ec
Magnus <magnus@hvass-labs.org>
2022-01-26 12:51:18 +0000

    random.py added random portfolio groups

--- investops/random.py
+++ investops/random.py
@@ -21,7 +21,7 @@ from investops.check import fix_corr_matrix
 ###############################################################################
 # Generate default names.
 
-def gen_names_assets(num_assets):
+def gen_asset_names(num_assets):
     """
     Generate list of strings with default asset-names.
 
@@ -31,7 +31,7 @@ def gen_names_assets(num_assets):
     return [f'Asset {i}' for i in range(num_assets)]
 
 
-def gen_names_groups(num_groups):
+def gen_group_names(num_groups):
     """
     Generate list of strings with default group-names.
 
@@ -61,7 +61,7 @@ def _named_weights(weights, num_assets, names):
     if names is not None:
         if names is True:
             # Use default asset-names.
-            names = gen_names_assets(num_assets=num_assets)
+            names = gen_asset_names(num_assets=num_assets)
         elif isinstance(names, list):
             # Use the list of names as-is, assuming they are all strings.
             # Check the number of names is correct.
@@ -188,5 +188,146 @@ def rand_zero(rng, a, prob):
 
     # Create a copy of the input array where random values are set to zero.
     return np.where(p < prob, 0, a)
+
+###############################################################################
+# Random portfolio groups.
+
+def rand_groups(rng, num_assets, num_groups,
+                max_groups_per_asset, min_groups_per_asset=0,
+                asset_names=None, group_names=None):
+    """
+    Generate dict that maps from asset-names to lists of random group-names.
+
+    This is intended to be used with the class `GroupConstraints`.
+
+    :param rng: `Numpy.random.Generator` object from `np.random.default_rng()`
+    :param num_assets: Integer with the number of portfolio assets.
+    :param num_groups: Integer with the number of portfolio groups.
+    :param max_groups_per_asset: Int with max number of groups per asset.
+    :param min_groups_per_asset: Int with min number of groups per asset.
+    :param asset_names:
+        If list then use those asset-names, otherwise use default asset-names.
+    :param group_names:
+        If list then use those group-names, otherwise use default group-names.
+    :return: Dict that maps asset-names to lists of group-names.
+    """
+    # Check number of groups is valid.
+    if min_groups_per_asset > num_groups or max_groups_per_asset > num_groups \
+            or min_groups_per_asset > max_groups_per_asset:
+        msg = f'Arguments \'min_groups_per_asset\'={min_groups_per_asset}, ' \
+              f'\'max_groups_per_asset\'={max_groups_per_asset}, and ' \
+              f'\'num_groups\'={num_groups} are invalid.'
+        raise ValueError(msg)
+
+    # Asset-names.
+    if not isinstance(asset_names, list):
+        # Generate list of default asset-names.
+        asset_names = gen_asset_names(num_assets=num_assets)
+    elif len(asset_names) != num_assets:
+        # The list of given asset-names has the wrong length.
+        msg = f'Argument \'asset_names\' has wrong length {len(asset_names)} ' \
+              f'expected {num_assets}.'
+        raise ValueError(msg)
+
+    # Group-names.
+    if not isinstance(group_names, list):
+        # Generate list of default group-names.
+        group_names = gen_group_names(num_groups=num_groups)
+    elif len(group_names) != num_groups:
+        # The list of given group-names has the wrong length.
+        msg = f'Argument \'group_names\' has wrong length {len(group_names)} ' \
+              f'expected {num_groups}.'
+        raise ValueError(msg)
+
+    # Initialize the dict that we will build.
+    asset_to_groups = dict()
+
+    # For each asset-name in the portfolio.
+    for asset_name in asset_names:
+        # Random number of groups to select.
+        num_groups = rng.integers(low=min_groups_per_asset,
+                                  high=max_groups_per_asset + 1)
+
+        # Random selection of group-names.
+        groups = rng.choice(group_names, size=num_groups, replace=False)
+
+        # Sort the group-names.
+        groups.sort()
+
+        # Add this asset and list of group-names to the dict.
+        asset_to_groups[asset_name] = groups.tolist()
+
+    return asset_to_groups
+
+
+def rand_group_lim_uniform(rng, num_groups,
+                           min_lim=0.0, max_lim=1.0, names=None):
+    """
+    Generate random portfolio group-limits using a uniform distribution.
+
+    This is intended to be used with the class `GroupConstraints`.
+
+    :param rng: `Numpy.random.Generator` object from `np.random.default_rng()`
+    :param num_groups: Integer with the number of portfolio groups.
+    :param min_lim: Minimum group-limit.
+    :param max_lim: Maximum group-limit.
+    :param names:
+        If list then use those group-names, otherwise use default group-names.
+    :return: Pandas Series with random group-limits.
+    """
+    if not isinstance(names, list):
+        # Generate list of default group-names.
+        names = gen_group_names(num_groups=num_groups)
+    elif len(names) != num_groups:
+        # The list of given group-names has the wrong length.
+        msg = f'Argument \'names\' has wrong length {len(names)} ' \
+              f'expected {num_groups}.'
+        raise ValueError(msg)
+
+    # Generate random group-limits.
+    group_lim = rng.uniform(low=min_lim, high=max_lim, size=num_groups)
+
+    # Convert Numpy array to Pandas Series.
+    group_lim = pd.Series(data=group_lim, index=names)
+
+    return group_lim
+
+
+def rand_group_lim_normal(rng, num_groups, mean=0.0, std=0.04,
+                          min_lim=0.0, max_lim=1.0, names=None):
+    """
+    Generate random portfolio group-limits using a normal distribution.
+
+    This is intended to be used with the class `GroupConstraints`.
+
+    :param rng: `Numpy.random.Generator` object from `np.random.default_rng()`
+    :param num_groups: Integer with the number of portfolio groups.
+    :param mean: Mean of the normal distribution.
+    :param std: Std.dev. of the normal distribution.
+    :param min_lim: Minimum group-limit.
+    :param max_lim: Maximum group-limit.
+    :param names:
+        If list then use those group-names, otherwise use default group-names.
+    :return: Pandas Series with random group-limits.
+    """
+    if not isinstance(names, list):
+        # Generate list of default group-names.
+        names = gen_group_names(num_groups=num_groups)
+    elif len(names) != num_groups:
+        # The list of given group-names has the wrong length.
+        msg = f'Argument \'names\' has wrong length {len(names)} ' \
+              f'expected {num_groups}.'
+        raise ValueError(msg)
+
+    # Generate random group-limits.
+    group_lim = rng.normal(scale=std, loc=mean, size=num_groups)
+
+    # Clip the group-limits.
+    group_lim = np.clip(group_lim, min_lim, max_lim)
+
+    # Convert Numpy array to Pandas Series.
+    group_lim = pd.Series(data=group_lim, index=names)
+
+    return group_lim
 
 ###############################################################################
