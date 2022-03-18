a620bee1dea672c2d54d30c382a0bb4adfea87d2
Magnus <magnus@hvass-labs.org>
2022-03-18 16:33:18 +0000

    Added constants.py

---
+++ investops/constants.py
@@ -0,0 +1,33 @@
+###############################################################################
+#
+# Various constants.
+#
+###############################################################################
+#
+# This file is part of InvestOps:
+#
+# https://github.com/Hvass-Labs/InvestOps
+#
+# Published under the MIT License. See the file LICENSE for details.
+#
+# Copyright 2022 by Magnus Erik Hvass Pedersen
+#
+###############################################################################
+
+#: Average number of business- or trading-days in a year.
+BDAYS_PER_YEAR = 251.67
+
+#: Average number of week-days in a year.
+#: Normal years have 365 days, but every 4th year is a leap-year with 366 days.
+DAYS_PER_YEAR = 365.25
+
+#: Number of weeks per year.
+WEEKS_PER_YEAR = 52
+
+#: Number of months per year.
+MONTHS_PER_YEAR = 12
+
+#: Number of quarters per year.
+QUARTERS_PER_YEAR = 4
+
+###############################################################################
